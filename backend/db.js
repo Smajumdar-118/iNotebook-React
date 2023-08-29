5b3a1d82c706902fc617e3b82386aaf50d78fd8f
Sayan <sayanmajumdar4444@gmail.com>
2023-08-29 23:33:28 +0530

    Transfered local database to cloud

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 // const mongoURI = "mongodb://localhost:27017/iNotebook"
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebook"
+// const mongoURI = "mongodb://127.0.0.1:27017/iNotebook"
+const mongoURI = "mongodb+srv://Smajumdar_118:Sayan4444@mydb.xpjoxqo.mongodb.net/"
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURI);
