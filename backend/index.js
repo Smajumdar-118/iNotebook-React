30b6e5dab8c7e6e24df28c647039ae6f25ec49b2
Ubuntu <ubuntu@ip-172-31-42-217.ap-south-1.compute.internal>
2025-10-25 14:02:29 +0000

    Fixed Login issue in EC2

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ const host = '127.0.0.1';
 app.use(cors())
 
 app.use(express.json())
-app.use('/api/auth' , require('./routes/auth'));
-app.use('/api/notes' , require('./routes/notes'));
+app.use('/auth' , require('./routes/auth'));
+app.use('/notes' , require('./routes/notes'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
